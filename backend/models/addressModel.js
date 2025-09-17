ad9abc031328147ff3f45613ad25b3f20abb604b
Ansibatk <ansibatkh98@gmail.com>
2025-09-17 14:24:52 +0530

    new rules added to schema

--- backend/models/addressModel.js
+++ backend/models/addressModel.js
@@ -1,27 +1,40 @@
-const mongoose=require('mongoose')
+import mongoose from "mongoose";
 const addressSchema=new mongoose.Schema({
     id:{
         type:String,
     },
     type:{
         type:String,
+        required: true,
+        trim: true
     },
     street:{
         type:String,
+        required: true,
+        trim: true
     },
     city:{
         type:String,
+        required: true,
+        trim: true
     },
     state:{
         type:String,
+        required: true,
+        trim: true
     },
     country:{
         type:String,
+        required: true,
+        trim: true
     },
     pincode:{
         type:Number,
-    },
-    
-    })
+        required:true,
+ }
+},
+{
+    timestamps:true
+});
     const Address=mongoose.model('Address',addressSchema)
-    module.exports=Address;
+   export default Address;
