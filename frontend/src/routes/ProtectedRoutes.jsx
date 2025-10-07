3b5fb7a4696b83de460c26180babb8056a98de5e
Ansibatk <ansibatkh98@gmail.com>
2025-10-07 19:13:30 +0530

    initial commit

---
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./Context/AuthContext";
+
+const ProtectedRoutes=({children, allowedRoles})=>{
+    const {token, userRole}=useAuth();
+  if(!token) return <Navigate to ="/login"/>
+// role based access control
+if(allowedRoles && !allowedRoles.includes(userRole))
+{
+    return <Navigate to ="/login"/>
+}
+    return children;
+}
+export default ProtectedRoutes
